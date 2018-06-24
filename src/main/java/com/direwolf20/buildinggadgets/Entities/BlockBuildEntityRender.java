0f91e3fde7a1d43cd73041782b8cb2506e50ae35
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2018-06-23 22:24:16 -0400

    Blue cube around glowing blocks

--- src/main/java/com/direwolf20/buildinggadgets/Entities/BlockBuildEntityRender.java
+++ src/main/java/com/direwolf20/buildinggadgets/Entities/BlockBuildEntityRender.java
@@ -32,12 +32,14 @@ public class BlockBuildEntityRender extends Render<BlockBuildEntity> {
     @Override
     public void doRender(BlockBuildEntity entity, double x, double y, double z, float entityYaw, float partialTicks) {
 
+
+
         BlockRendererDispatcher blockrendererdispatcher = Minecraft.getMinecraft().getBlockRendererDispatcher();
 
         Minecraft mc = Minecraft.getMinecraft();
         GlStateManager.pushMatrix();
 
-        //GlStateManager.scale(1.01F, 1.01F, 1.01F);
+                //GlStateManager.scale(1.01F, 1.01F, 1.01F);
         /*float scale2 = 0.025f;
         GlStateManager.translate((1-scale2)/2,(1-scale2)/2,(1-scale2)/2);
         GlStateManager.scale(scale2,scale2,scale2);
@@ -58,6 +60,73 @@ public class BlockBuildEntityRender extends Render<BlockBuildEntity> {
         IBlockState renderBlockState = Blocks.COBBLESTONE.getDefaultState();
         blockrendererdispatcher.renderBlockBrightness(renderBlockState, 1.0f);
         GlStateManager.popMatrix();
+
+        GlStateManager.pushMatrix();
+        GlStateManager.pushAttrib();
+
+        GlStateManager.enableBlend();
+        GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+        //GlStateManager.glLineWidth(1.0f);
+        GlStateManager.disableTexture2D();
+        GlStateManager.depthMask(false);
+        Tessellator t = Tessellator.getInstance();
+        BufferBuilder bufferBuilder = t.getBuffer();
+        bufferBuilder.begin(7, DefaultVertexFormats.POSITION_COLOR);
+        double minX = x;
+        double minY = y;
+        double minZ = z;
+        double maxX = x+1;
+        double maxY = y+1;
+        double maxZ = z+1;
+        float red = 0f;
+        float green = 1f;
+        float blue = 1f;
+        float alpha = (1f-(scale));
+        if (alpha <0.25f) {alpha = 0.25f;}
+        //down
+        bufferBuilder.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+
+        //up
+        bufferBuilder.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+
+        //north
+        bufferBuilder.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
+
+        //south
+        bufferBuilder.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+
+        //east
+        bufferBuilder.pos(maxX, minY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(maxX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+
+        //west
+        bufferBuilder.pos(minX, minY, minZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(minX, minY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(minX, maxY, maxZ).color(red, green, blue, alpha).endVertex();
+        bufferBuilder.pos(minX, maxY, minZ).color(red, green, blue, alpha).endVertex();
+        t.draw();
+
+        GlStateManager.disableBlend();
+        GlStateManager.enableTexture2D();
+        GlStateManager.depthMask(true);
+
+        GlStateManager.popMatrix();
+        GlStateManager.popAttrib();
+
     }
 
     @Nullable
