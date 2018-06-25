5a45b6fd15a18fef119279c3576d8f9ed784af0f
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2018-06-25 19:55:19 -0400

    BuildToMe Fully Implemented

--- src/main/java/com/direwolf20/buildinggadgets/Items/BuildingTool.java
+++ src/main/java/com/direwolf20/buildinggadgets/Items/BuildingTool.java
@@ -11,6 +11,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
@@ -35,9 +36,10 @@ public class BuildingTool extends Item {
         ItemStack itemstack = player.getHeldItem(hand);
         player.setActiveHand(hand);
         RayTraceResult lookingAt = player.rayTrace(20, 1.0F);
+        //System.out.println(lookingAt.sideHit);
         if (!world.isRemote) {
             if (world.getBlockState(lookingAt.getBlockPos()) != Blocks.AIR.getDefaultState()) {
-                buildToMe(world, player, lookingAt.getBlockPos());
+                buildToMe(world, player, lookingAt.getBlockPos(),lookingAt.sideHit);
                 //world.spawnEntity(new BlockBuildEntity(world, lookingAt.getBlockPos().up(), player,Blocks.COBBLESTONE.getDefaultState()));
             }
         }
@@ -47,19 +49,52 @@ public class BuildingTool extends Item {
         return new ActionResult<ItemStack>(EnumActionResult.SUCCESS, itemstack);
     }
 
-    public boolean buildToMe(World world, EntityPlayer player, BlockPos startBlock) {
+    public boolean buildToMe(World world, EntityPlayer player, BlockPos startBlock, EnumFacing sideHit) {
         BlockPos playerPos = player.getPosition();
-        Block airBlock = Blocks.AIR;
         IBlockState cobbleBlock = Blocks.COBBLESTONE.getDefaultState();
         BlockPos changePos;
-        if (playerPos.getX() == startBlock.getX()) {
-            for (int i = startBlock.getZ();i<= playerPos.getZ();i++) {
-                changePos = new BlockPos(startBlock.getX(),startBlock.getY(),i);
-                if (world.getBlockState(changePos) == airBlock.getDefaultState()) {
-                    world.spawnEntity(new BlockBuildEntity(world, changePos, player,cobbleBlock,false));
-                }
-
+        if (sideHit == EnumFacing.SOUTH) {
+            for (int i = startBlock.getZ(); i <= playerPos.getZ(); i++) {
+                changePos = new BlockPos(startBlock.getX(), startBlock.getY(), i);
+                placeBlock(world, player, changePos, cobbleBlock);
             }
+        }
+        else if (sideHit == EnumFacing.NORTH) {
+            for (int i = startBlock.getZ(); i >= playerPos.getZ(); i--) {
+                changePos = new BlockPos(startBlock.getX(), startBlock.getY(), i);
+                placeBlock(world, player, changePos, cobbleBlock);
+            }
+        }
+        else if (sideHit == EnumFacing.EAST) {
+            for (int i = startBlock.getX(); i <= playerPos.getX(); i++) {
+                changePos = new BlockPos(i, startBlock.getY(), startBlock.getZ());
+                placeBlock(world, player, changePos, cobbleBlock);
+            }
+        }
+        else if (sideHit == EnumFacing.WEST) {
+            for (int i = startBlock.getX(); i >= playerPos.getX(); i--) {
+                changePos = new BlockPos(i, startBlock.getY(), startBlock.getZ());
+                placeBlock(world, player, changePos, cobbleBlock);
+            }
+        }
+        else if (sideHit == EnumFacing.UP) {
+            for (int i = startBlock.getY(); i <= playerPos.getY(); i++) {
+                changePos = new BlockPos(startBlock.getX(), i, startBlock.getZ());
+                placeBlock(world, player, changePos, cobbleBlock);
+            }
+        }
+        else if (sideHit == EnumFacing.DOWN) {
+            for (int i = startBlock.getY(); i >= playerPos.getY(); i--) {
+                changePos = new BlockPos(startBlock.getX(), i, startBlock.getZ());
+                placeBlock(world, player, changePos, cobbleBlock);
+            }
+        }
+        return true;
+    }
+
+    public boolean placeBlock(World world, EntityPlayer player, BlockPos pos, IBlockState setBlock) {
+        if (world.getBlockState(pos).getBlock().isReplaceable(world,pos)) {
+            world.spawnEntity(new BlockBuildEntity(world, pos, player, setBlock,false));
         }
         return true;
     }
