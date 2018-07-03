0162cc1544983b53ff8f7d459ba9179c8f212e8c
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2018-07-03 14:25:18 -0400

    Version Change

--- src/main/java/com/direwolf20/buildinggadgets/BuildingGadgets.java
+++ src/main/java/com/direwolf20/buildinggadgets/BuildingGadgets.java
@@ -9,16 +9,12 @@ import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.apache.logging.log4j.Logger;
 
 
-@Mod(modid = BuildingGadgets.MODID, name = BuildingGadgets.MODNAME,
-        //dependencies =
-        //        "required-after:botania;required-after:baubles;",
-
-        version = BuildingGadgets.VERSION, useMetadata = true)
+@Mod(modid = BuildingGadgets.MODID, name = BuildingGadgets.MODNAME,version = BuildingGadgets.VERSION, useMetadata = true)
 
 public class BuildingGadgets {
     public static final String MODID = "buildinggadgets";
     public static final String MODNAME = "Building Gadgets";
-    public static final String VERSION = "1.0.2";
+    public static final String VERSION = "1.0.3";
 
 
     @SidedProxy(clientSide = "com.direwolf20.buildinggadgets.ClientProxy", serverSide = "com.direwolf20.buildinggadgets.ServerProxy")
