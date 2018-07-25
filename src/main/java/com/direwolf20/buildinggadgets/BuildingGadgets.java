b980d78e49a0a32c7517cedaed7b5d9230dd5862
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2018-07-25 17:08:36 -0400

    1.5.1

--- src/main/java/com/direwolf20/buildinggadgets/BuildingGadgets.java
+++ src/main/java/com/direwolf20/buildinggadgets/BuildingGadgets.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 public class BuildingGadgets {
     public static final String MODID = "buildinggadgets";
     public static final String MODNAME = "Building Gadgets";
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.5.1";
 
 
     @SidedProxy(clientSide = "com.direwolf20.buildinggadgets.ClientProxy", serverSide = "com.direwolf20.buildinggadgets.ServerProxy")
