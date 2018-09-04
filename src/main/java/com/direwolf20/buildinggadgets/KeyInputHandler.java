86b89cb249f5a42a957f6cc70834e0652b2f7637
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2018-09-03 23:22:24 -0400

    Support for mouse keybinds i think?

--- src/main/java/com/direwolf20/buildinggadgets/KeyInputHandler.java
+++ src/main/java/com/direwolf20/buildinggadgets/KeyInputHandler.java
@@ -37,4 +37,28 @@ public class KeyInputHandler {
         }
 
     }
+
+    @SubscribeEvent
+    public void onMouseInput(InputEvent.MouseInputEvent event) {
+        //if (KeyBindings.modeSwitch.isPressed()) {
+        if (KeyBindings.modeSwitch.isKeyDown()) {
+            //PacketHandler.INSTANCE.sendToServer(new PacketToggleMode());
+            Minecraft mc = Minecraft.getMinecraft();
+            ItemStack stack = mc.player.getHeldItem(EnumHand.MAIN_HAND);
+            if (!stack.isEmpty() && ((stack.getItem() instanceof GenericGadget)))
+                mc.displayGuiScreen(new ModeRadialMenu(stack));
+            else {
+                stack = mc.player.getHeldItem(EnumHand.OFF_HAND);
+                if (!stack.isEmpty() && ((stack.getItem() instanceof GenericGadget)))
+                    mc.displayGuiScreen(new ModeRadialMenu(stack));
+            }
+        } else if (KeyBindings.rangeChange.isPressed()) {
+            PacketHandler.INSTANCE.sendToServer(new PacketChangeRange());
+        } else if (KeyBindings.undoKey.isPressed()) {
+            PacketHandler.INSTANCE.sendToServer(new PacketUndoKey());
+        } else if (KeyBindings.anchorKey.isPressed()) {
+            PacketHandler.INSTANCE.sendToServer(new PacketAnchorKey());
+        }
+
+    }
 }
