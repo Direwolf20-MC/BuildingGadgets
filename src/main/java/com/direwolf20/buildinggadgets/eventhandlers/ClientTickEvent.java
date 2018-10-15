1f6dd67ad39a1955b9ee8014afbdead6030ca54f
Phylogeny <molecularphylo@gmail.com>
2018-10-15 10:17:21 -0700

    Continue attempting to send block map client sync packet upon joining a world, until successful. This fixes the issue where copy/paste tool in-world preview doesn't immediately render when first joining a world.

--- src/main/java/com/direwolf20/buildinggadgets/eventhandlers/ClientTickEvent.java
+++ src/main/java/com/direwolf20/buildinggadgets/eventhandlers/ClientTickEvent.java
@@ -11,17 +11,19 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import net.minecraftforge.fml.common.network.FMLNetworkEvent.ClientConnectedToServerEvent;
 import net.minecraftforge.fml.relauncher.Side;
 
 @EventBusSubscriber(Side.CLIENT)
 public class ClientTickEvent {
 
     private static int counter = 0;
+    private static boolean joinedWorld;
 
     @SubscribeEvent
     public static void onClientTick(@SuppressWarnings("unused") TickEvent.ClientTickEvent event) {
         counter++;
-        if (counter > 600) {
+        if (counter > 600 || !joinedWorld) {
             counter = 0;
             EntityPlayer player = Minecraft.getMinecraft().player;
             if (player == null) return;
@@ -35,8 +37,14 @@ public class ClientTickEvent {
                 if (UUID != null && PasteToolBufferBuilder.isUpdateNeeded(UUID, stack)) {
                     //System.out.println("BlockMap Update Needed for UUID: " + UUID + " in slot " + i);
                     PacketHandler.INSTANCE.sendToServer(new PacketRequestBlockMap(template.getUUID(stack), !(template instanceof CopyPasteTool)));
+                    joinedWorld = true;
                 }
             }
         }
+    }
+
+    @SubscribeEvent
+    public static void onJoinWorld(@SuppressWarnings("unused") ClientConnectedToServerEvent event) {
+        joinedWorld = false;
     }
 }
