d9b96c44a25cfcf02cf03b9f16cee47b4ace09ac
Phylogeny <molecularphylo@gmail.com>
2018-10-23 03:51:24 -0700

    Fixed bug where template manger slots were mapped to the wrong player inventory slots.

--- src/main/java/com/direwolf20/buildinggadgets/blocks/templatemanager/TemplateManagerContainer.java
+++ src/main/java/com/direwolf20/buildinggadgets/blocks/templatemanager/TemplateManagerContainer.java
@@ -33,7 +33,7 @@ public class TemplateManagerContainer extends Container {
             for (int col = 0; col < 9; ++col) {
                 int x = 8 + col * 18;
                 int y = row * 18 + 84;
-                this.addSlotToContainer(new Slot(playerInventory, col + row * 9 + 10, x, y));
+                this.addSlotToContainer(new Slot(playerInventory, col + row * 9 + 9, x, y));
             }
         }
 
