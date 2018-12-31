81b1ce685ecd9ffdaa9706f5008465e1e0a8f42b
Major Tuvok <kevin.iselborn@t-online.de>
2018-12-31 10:08:49 +0100

    Shorten ReflectionTools Code, now it's finished

--- src/main/java/com/direwolf20/buildinggadgets/common/tools/ReflectionTool.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/tools/ReflectionTool.java
@@ -6,8 +6,10 @@ import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
+import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class ReflectionTool {
     public static final Predicate<Field> PREDICATE_STATIC = field -> Modifier.isStatic(field.getModifiers());
@@ -19,14 +21,7 @@ public class ReflectionTool {
      * @return A List containing the Fields declared by this class (superclasses don't count!) who match the given Predicate
      */
     public static List<Field> getFilteredFields(Class<?> clazz, Predicate<Field> filter) {
-        Field[] fields = clazz.getDeclaredFields();
-        ImmutableList.Builder<Field> res = ImmutableList.builder();
-        for (Field field:
-             fields) {
-            if (filter.test(field))
-                res.add(field);
-        }
-        return res.build();
+        return Stream.of(clazz.getDeclaredFields()).filter(filter).collect(ImmutableList.toImmutableList());
     }
 
     /**
@@ -36,6 +31,6 @@ public class ReflectionTool {
      * @return whether or not the instance is null if the Field is static, or non-null (and of an appropriate class) if the Field is not
      */
     public static boolean isInstanceProvidedForField(@Nonnull Field field, @Nullable Object instance) {
-        return (instance != null && !ReflectionTool.PREDICATE_STATIC.test(field) && field.getType().isAssignableFrom(instance.getClass())) || (instance == null && ReflectionTool.PREDICATE_STATIC.test(field));
+        return (instance != null && !ReflectionTool.PREDICATE_STATIC.test(field) && field.getDeclaringClass().isAssignableFrom(instance.getClass())) || (instance == null && ReflectionTool.PREDICATE_STATIC.test(field));
     }
 }
