3ebf37151a4714392cfd74b55dd6b1c27ffeccbd
hnOsmium0001 <osmium_0000@163.com>
2019-02-19 13:38:16 -0800

    standalone and better Region

--- src/main/java/com/direwolf20/buildinggadgets/common/building/Region.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/building/Region.java
@@ -1,13 +1,17 @@
 package com.direwolf20.buildinggadgets.common.building;
 
 import com.direwolf20.buildinggadgets.common.building.placement.IPlacementSequence;
+import com.google.common.base.MoreObjects;
 import com.google.common.collect.AbstractIterator;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
@@ -15,31 +19,47 @@ import java.util.function.Predicate;
  * Represents a region in the world with a finite and nonzero size.
  * <p>Javadoc are copied from {@link AxisAlignedBB} with some modifications.</p>
  */
-public final class Region extends StructureBoundingBox implements IPlacementSequence {
+public final class Region implements IPlacementSequence, Comparable<Region> {
+
+    private int minX, minY, minZ;
+    private int maxX, maxY, maxZ;
 
     public Region(int[] vertexes) {
-        super(vertexes);
+        this(vertexes[0], vertexes[1], vertexes[2], vertexes[3], vertexes[4], vertexes[5]);
     }
 
     public Region(int minX, int minY, int minZ, int maxX, int maxY, int maxZ) {
-        super(minX, minY, minZ, maxX, maxY, maxZ);
+        this.minX = minX;
+        this.minY = minY;
+        this.minZ = minZ;
+        this.maxX = maxX;
+        this.maxY = maxY;
+        this.maxZ = maxZ;
     }
 
     public Region(Vec3i vertex) {
         this(vertex, vertex);
     }
 
-    public Region(Vec3i mincVertex, Vec3i maxVertex) {
-        super(mincVertex, maxVertex);
+    public Region(Vec3i minVertex, Vec3i maxVertex) {
+        this(minVertex.getX(), minVertex.getY(), minVertex.getZ(), maxVertex.getX(), maxVertex.getY(), maxVertex.getZ());
     }
 
+    /**
+     * @return this
+     */
     public Region offsetBy(int x, int y, int z) {
-        super.offset(x, y, z);
+        this.minX += x;
+        this.minY += y;
+        this.minZ += z;
+        this.maxX += x;
+        this.maxY += y;
+        this.maxZ += z;
         return this;
     }
 
     /**
-     * @see #offsetBy(Vec3i)
+     * @see #offsetBy(int, int, int)
      */
     public Region offsetBy(Vec3i direction) {
         return this.offsetBy(direction.getX(), direction.getY(), direction.getZ());
@@ -132,6 +152,15 @@ public final class Region extends StructureBoundingBox implements IPlacementSequ
         return new Region(minX, minY, minZ, maxX, maxY, maxZ);
     }
 
+    public boolean intersectsWith(Region other) {
+        return this.maxX >= other.minX &&
+                this.minX <= other.maxX &&
+                this.maxZ >= other.minZ &&
+                this.minZ <= other.maxZ &&
+                this.maxY >= other.minY &&
+                this.minY <= other.maxY;
+    }
+
     /**
      * Create a new region that encloses both regions that has the minimum volume.
      *
@@ -155,6 +184,10 @@ public final class Region extends StructureBoundingBox implements IPlacementSequ
         return this;
     }
 
+    public StructureBoundingBox toStructureBoundingBox() {
+        return new StructureBoundingBox(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+
     public boolean isXInBound(int x) {
         return x >= minX && x <= maxX;
     }
@@ -169,6 +202,54 @@ public final class Region extends StructureBoundingBox implements IPlacementSequ
 
     public boolean isInBound(int x, int y, int z) {
         return isXInBound(x) && isYInBound(y) && isZInBound(z);
+    }
+
+    public boolean isInBound(Vec3i vec) {
+        return isInBound(vec.getX(), vec.getY(), vec.getZ());
+    }
+
+    public int getMinX() {
+        return minX;
+    }
+
+    public int getMinY() {
+        return minY;
+    }
+
+    public int getMinZ() {
+        return minZ;
+    }
+
+    public int getMaxX() {
+        return maxX;
+    }
+
+    public int getMaxY() {
+        return maxY;
+    }
+
+    public int getMaxZ() {
+        return maxZ;
+    }
+
+    public BlockPos getMinVec() {
+        return new BlockPos(minX, minY, minZ);
+    }
+
+    public BlockPos getMaxVec() {
+        return new BlockPos(maxX, maxY, maxZ);
+    }
+
+    public int getXSize() {
+        return maxX - minX + 1;
+    }
+
+    public int getYSize() {
+        return maxY - minY + 1;
+    }
+
+    public int getZSize() {
+        return maxZ - minZ + 1;
     }
 
     public int getSize() {
@@ -255,6 +336,76 @@ public final class Region extends StructureBoundingBox implements IPlacementSequ
      */
     public Region copy() {
         return new Region(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("minX", minX)
+                .add("minY", minY)
+                .add("minZ", minZ)
+                .add("maxX", maxX)
+                .add("maxY", maxY)
+                .add("maxZ", maxZ)
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Region blockPos = (Region) o;
+        return minX == blockPos.minX &&
+                minY == blockPos.minY &&
+                minZ == blockPos.minZ &&
+                maxX == blockPos.maxX &&
+                maxY == blockPos.maxY &&
+                maxZ == blockPos.maxZ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(minX, minY, minZ, maxX, maxY, maxZ);
+    }
+
+    @Override
+    public int compareTo(Region that) {
+        if (this.minX < that.minX) {
+            return -1;
+        } else if (this.minX > that.minX) {
+            return 1;
+        }
+
+        if (this.minZ < that.minZ) {
+            return -1;
+        } else if (this.minZ > that.minZ) {
+            return 1;
+        }
+
+        if (this.minY < that.minY) {
+            return -1;
+        } else if (this.minY > that.minY) {
+            return 1;
+        }
+
+        if (this.maxX < that.maxX) {
+            return -1;
+        } else if (this.maxX > that.maxX) {
+            return 1;
+        }
+
+        if (this.maxZ < that.maxZ) {
+            return -1;
+        } else if (this.maxZ > that.maxZ) {
+            return 1;
+        }
+
+        if (this.maxY < that.maxY) {
+            return -1;
+        } else if (this.maxY > that.maxY) {
+            return 1;
+        }
+        return 0;
     }
 
 }
