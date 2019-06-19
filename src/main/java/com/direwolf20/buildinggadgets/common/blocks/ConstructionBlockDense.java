ac45130185f148a44d2464b304fd140432c407ce
direwolf20 <39863894+Direwolf20-MC@users.noreply.github.com>
2019-06-18 20:06:16 -0400

    Added drops to Dense construction paste, not sure if this is loot tables though?

--- src/main/java/com/direwolf20/buildinggadgets/common/blocks/ConstructionBlockDense.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/blocks/ConstructionBlockDense.java
@@ -1,6 +1,15 @@
 package com.direwolf20.buildinggadgets.common.blocks;
 
+import com.direwolf20.buildinggadgets.common.config.Config;
+import com.direwolf20.buildinggadgets.common.registry.objects.BGItems;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.storage.loot.LootContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class ConstructionBlockDense extends Block {
 
@@ -8,6 +17,14 @@ public class ConstructionBlockDense extends Block {
         super(properties);
     }
 
+    @Override
+    public List<ItemStack> getDrops(BlockState state, LootContext.Builder builder) {
+        int min = Config.GENERAL.pasteDroppedMin.get();
+        Random random = new Random();
+        return new ArrayList<ItemStack>() {{ //TODO change to LootTable and ensure Dire didn't break me
+            add(new ItemStack(BGItems.constructionPaste, min + random.nextInt(Config.GENERAL.pasteDroppedMax.get() - min + 1)));
+        }};
+    }
 // fixme: we need loot tables now.
 //    @Override
 //    public IItemProvider getItemDropped(BlockState state, World world, BlockPos pos, int fortune) {
