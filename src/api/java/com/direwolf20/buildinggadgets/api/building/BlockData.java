387c7b619a50eac25f5e468ec1a11af28821f108
Major_Tuvok <kevin.iselborn@t-online.de>
2019-07-25 20:11:58 +0200

    Merge quick change on api over just before release.

--- src/api/java/com/direwolf20/buildinggadgets/api/building/BlockData.java
+++ src/api/java/com/direwolf20/buildinggadgets/api/building/BlockData.java
@@ -158,6 +158,7 @@ public final class BlockData {
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
+
         if (! (o instanceof BlockData)) return false;
 
         BlockData blockData = (BlockData) o;
