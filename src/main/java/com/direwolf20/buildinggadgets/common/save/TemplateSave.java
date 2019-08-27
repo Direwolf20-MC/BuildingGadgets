48254b85b7736bffb19c62c40a4ef10af6dcc14e
Major_Tuvok <kevin.iselborn@t-online.de>
2019-08-27 12:29:26 +0200

    Implement loading of Templates - I do allow sub-directories which will override whatever is in the super-directory. I also improved saving, such that it clears out the save-required Status

--- src/main/java/com/direwolf20/buildinggadgets/common/save/TemplateSave.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/save/TemplateSave.java
@@ -1,17 +1,22 @@
 package com.direwolf20.buildinggadgets.common.save;
 
+import com.direwolf20.buildinggadgets.api.serialisation.TemplateHeader;
 import com.direwolf20.buildinggadgets.api.template.DelegatingTemplate;
 import com.direwolf20.buildinggadgets.api.template.ITemplate;
 import com.direwolf20.buildinggadgets.api.template.ImmutableTemplate;
 import com.direwolf20.buildinggadgets.api.template.TemplateIO;
 import com.direwolf20.buildinggadgets.common.BuildingGadgets;
 import com.direwolf20.buildinggadgets.common.util.ref.Reference;
+import jdk.internal.jline.internal.Nullable;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -68,28 +73,42 @@ public final class TemplateSave {
         String fileName = id.toString();
         Path templateFile = saveFolder.resolve(fileName + Reference.TEMPLATE_FILE_ENDING);
         Path templateHeaderFile = saveFolder.resolve(fileName + Reference.TEMPLATE_HEADER_FILE_ENDING);
-        try {
-            if (! Files.exists(templateFile))
-                Files.createFile(templateFile);
-            try (OutputStream templateFileStream = Files.newOutputStream(templateFile)) {
-                templateInfo.writeTemplateTo(templateFileStream);
-            }
-        } catch (IOException e) {
-            BuildingGadgets.LOG.error("Failed to save {}{}!", id, Reference.TEMPLATE_FILE_ENDING, e);
-        }
-        try {
-            if (! Files.exists(templateHeaderFile))
-                Files.createFile(templateHeaderFile);
-            try (OutputStream templateHeaderFileStream = Files.newOutputStream(templateHeaderFile)) {
-                templateInfo.writeTemplateHeaderTo(templateHeaderFileStream);
-            }
-        } catch (IOException e) {
-            BuildingGadgets.LOG.error("Failed to save {}{}!", id, Reference.TEMPLATE_HEADER_FILE_ENDING, e);
-        }
+        templateInfo.saveTo(id, templateFile, templateHeaderFile);
     }
 
     public void loadAll() {
+        try {
+            Files.walkFileTree(saveFolder, new ReadTemplateVisitor());
+        } catch (IOException e) {
+            BuildingGadgets.LOG.error("An error occurred whilst loading Templates from {}. Loading was aborted!", saveFolder);
+        }
+    }
 
+    public void clear() {
+        idToTemplate.clear();
+    }
+
+    private void loadDataForId(UUID id, Path templateFile, Path headerFile) throws IOException {
+        TemplateHeader header = loadHeader(headerFile);
+        ITemplate template = loadTemplate(templateFile, header);
+        idToTemplate.put(id, new TemplateInfo(template));
+        BuildingGadgets.LOG.trace("Successfully loaded Template with id={} and {}.", id, header != null ? "Template Header" : "no Header");
+    }
+
+    @Nullable
+    private TemplateHeader loadHeader(Path headerFile) throws IOException {
+        if (headerFile != null) {
+            try (InputStream stream = Files.newInputStream(headerFile)) {
+                return TemplateIO.readTemplateHeaderJson(stream);
+            }
+        }
+        return null;
+    }
+
+    private ITemplate loadTemplate(Path templateFile, @Nullable TemplateHeader header) throws IOException {
+        try (InputStream stream = Files.newInputStream(templateFile)) {
+            return TemplateIO.readTemplate(stream, header);
+        }
     }
 
     private static final class TemplateInfo {
@@ -114,16 +133,57 @@ public final class TemplateSave {
             return this;
         }
 
-        private void writeTemplateTo(OutputStream stream) throws IOException {
-            TemplateIO.writeTemplate(template, stream);
+        private void saveTo(UUID id, Path templateFile, Path templateHeaderFile) {
+            writeTemplateTo(id, templateFile);
+            writeTemplateHeaderTo(id, templateHeaderFile);
+            this.saveRequired = false;
         }
 
-        private void writeTemplateHeaderTo(OutputStream stream) throws IOException {
-            TemplateIO.writeTemplateHeaderJson(template, stream);
+        private void writeTemplateTo(UUID id, Path templateFile) {
+            try {
+                if (! Files.exists(templateFile))
+                    Files.createFile(templateFile);
+                try (OutputStream stream = Files.newOutputStream(templateFile)) {
+                    TemplateIO.writeTemplate(template, stream);
+                }
+            } catch (IOException e) {
+                BuildingGadgets.LOG.error("Failed to save {}{}!", id, Reference.TEMPLATE_FILE_ENDING, e);
+            }
+        }
+
+        private void writeTemplateHeaderTo(UUID id, Path templateHeaderFile) {
+            try {
+                if (! Files.exists(templateHeaderFile))
+                    Files.createFile(templateHeaderFile);
+                try (OutputStream stream = Files.newOutputStream(templateHeaderFile)) {
+                    TemplateIO.writeTemplateHeaderJson(template, stream);
+                }
+            } catch (IOException e) {
+                BuildingGadgets.LOG.error("Failed to save {}{}!", id, Reference.TEMPLATE_HEADER_FILE_ENDING, e);
+            }
         }
     }
 
     private final class ReadTemplateVisitor extends SimpleFileVisitor<Path> {
-        
+        @Override
+        public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+            super.visitFile(file, attrs);//ensure file non-null
+            if (file.getFileName() == null)
+                throw new IOException("Attempted to read from an unnamed file!");
+            String name = file.getFileName().toString();
+            if (name.endsWith(Reference.TEMPLATE_FILE_ENDING)) {
+                String fileName = name.replace(Reference.TEMPLATE_FILE_ENDING, "");
+                try {
+                    UUID id = UUID.fromString(fileName);
+                    Path parent = file.getParent();
+                    loadDataForId(id, file, parent != null ? parent.resolve(fileName + Reference.TEMPLATE_HEADER_FILE_ENDING) : null);
+                } catch (IllegalArgumentException e) {
+                    BuildingGadgets.LOG.warn("Found non id-File {}. This will be ignored for loading Templates.", file);
+                } catch (IOException e) {
+                    BuildingGadgets.LOG.error("Failed to read Template from {}!", file, e);
+                }
+            }
+            return FileVisitResult.CONTINUE;
+        }
     }
 }
