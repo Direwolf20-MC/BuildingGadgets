5d99943aa0f5df58bdb4430a66cde95df8d106a0
Major_Tuvok <kevin.iselborn@t-online.de>
2019-09-20 20:40:33 +0200

    Prevent EffectBlock from crashing client, when the Render Data hasn't been received when the client attempts to render the Block. This is a temporary pseudo-fix for #375 - it no longer crashes, but the issue remains. That's why I put that log there, so that we are reminded and annoyed by it.

--- src/main/java/com/direwolf20/buildinggadgets/client/renderer/EffectBlockTER.java
+++ src/main/java/com/direwolf20/buildinggadgets/client/renderer/EffectBlockTER.java
@@ -1,5 +1,6 @@
 package com.direwolf20.buildinggadgets.client.renderer;
 
+import com.direwolf20.buildinggadgets.api.building.BlockData;
 import com.direwolf20.buildinggadgets.common.BuildingGadgets;
 import com.direwolf20.buildinggadgets.common.blocks.EffectBlock;
 import com.direwolf20.buildinggadgets.common.registry.objects.BGBlocks;
@@ -21,6 +22,13 @@ public class EffectBlockTER extends TileEntityRenderer<EffectBlockTileEntity> {
 
     @Override
     public void render(EffectBlockTileEntity tile, double x, double y, double z, float partialTicks, int destroyStage) {
+        BlockData renderData = tile.getRenderedBlock();
+        if (renderData == null) {
+            BuildingGadgets.LOG.error("Client did not receive Effect-Block Render Data. " +
+                    "This is an error we know about and will try to fix as soon as possible! " +
+                    "Aborting render for now to prevent Client-Crash.");
+            return;
+        }
         BlockRendererDispatcher blockrendererdispatcher = Minecraft.getInstance().getBlockRendererDispatcher();
         Minecraft mc = Minecraft.getInstance();
         GlStateManager.pushMatrix();
@@ -36,12 +44,13 @@ public class EffectBlockTER extends TileEntityRenderer<EffectBlockTileEntity> {
         if (toolMode == EffectBlock.Mode.REMOVE || toolMode == EffectBlock.Mode.REPLACE)
             scale = (float) (maxLife - teCounter) / maxLife;
         float trans = (1 - scale) / 2;
+
         GlStateManager.translated(x, y, z);
         GlStateManager.translatef(trans, trans, trans);
         GlStateManager.rotatef(-90.0F, 0.0F, 1.0F, 0.0F);
         GlStateManager.scalef(scale, scale, scale);
 
-        BlockState renderBlockState = tile.getRenderedBlock().getState();
+        BlockState renderBlockState = renderData.getState();
         if (tile.isUsingPaste() && toolMode == EffectBlock.Mode.PLACE)
             renderBlockState = BGBlocks.constructionBlockDense.getDefaultState();
 
