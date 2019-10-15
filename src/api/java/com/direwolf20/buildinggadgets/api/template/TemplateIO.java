e1e32c9ac989ba493de4881c3fc6ebb1fa403628
Major_Tuvok <kevin.iselborn@t-online.de>
2019-10-15 09:34:40 +0200

    Improve exceptions thrown by TemplateIO.

--- src/api/java/com/direwolf20/buildinggadgets/api/template/TemplateIO.java
+++ src/api/java/com/direwolf20/buildinggadgets/api/template/TemplateIO.java
@@ -2,6 +2,12 @@ package com.direwolf20.buildinggadgets.api.template;
 
 import com.direwolf20.buildinggadgets.api.Registries;
 import com.direwolf20.buildinggadgets.api.exceptions.IllegalTemplateFormatException;
+import com.direwolf20.buildinggadgets.api.exceptions.IllegalTemplateFormatException.IllegalTemplateNBTException.DataCannotBeReadException;
+import com.direwolf20.buildinggadgets.api.exceptions.IllegalTemplateFormatException.IllegalTemplateNBTException.MissingDataException;
+import com.direwolf20.buildinggadgets.api.exceptions.IllegalTemplateFormatException.IllegalTemplateNBTException.MissingSerializerException;
+import com.direwolf20.buildinggadgets.api.exceptions.IllegalTemplateFormatException.SerialisationException.CorruptTemplateStringException;
+import com.direwolf20.buildinggadgets.api.exceptions.IllegalTemplateFormatException.SerialisationException.DataCannotBeWrittenException;
+import com.direwolf20.buildinggadgets.api.exceptions.IllegalTemplateFormatException.SerialisationException.UnknownSerializerException;
 import com.direwolf20.buildinggadgets.api.serialisation.ITemplateSerializer;
 import com.direwolf20.buildinggadgets.api.serialisation.TemplateHeader;
 import com.direwolf20.buildinggadgets.api.util.NBTKeys;
@@ -20,14 +26,18 @@ public final class TemplateIO {
     private static final Gson GSON = TemplateHeader.appendHeaderSpecification(new GsonBuilder(), false, true).create();
     private TemplateIO() {}
 
-    public static void writeTemplate(ITemplate template, OutputStream stream) throws IOException {
+    public static void writeTemplate(ITemplate template, OutputStream stream) throws IllegalTemplateFormatException {
         writeTemplate(template, stream, true);
     }
 
-    public static void writeTemplate(ITemplate template, OutputStream stream, boolean persisted) throws IOException {
+    public static void writeTemplate(ITemplate template, OutputStream stream, boolean persisted) throws IllegalTemplateFormatException {
         CompoundNBT nbt = new CompoundNBT();
         writeTemplate(template, nbt, persisted);
-        CompressedStreamTools.writeCompressed(nbt, stream);
+        try {
+            CompressedStreamTools.writeCompressed(nbt, stream);
+        } catch (IOException e) {
+            throw new DataCannotBeWrittenException(e, template.getSerializer().getRegistryName(), nbt);
+        }
     }
 
     public static void writeTemplate(ITemplate template, CompoundNBT nbt, boolean persisted) {
@@ -41,7 +51,7 @@ public final class TemplateIO {
      * @see #readTemplate(InputStream, TemplateHeader, boolean)
      */
     @Nullable
-    public static ITemplate readTemplate(InputStream stream, @Nullable TemplateHeader header) throws IOException {
+    public static ITemplate readTemplate(InputStream stream, @Nullable TemplateHeader header) throws IllegalTemplateFormatException {
         return readTemplate(stream, header, true);
     }
 
@@ -53,8 +63,12 @@ public final class TemplateIO {
      * @throws IOException if a read error occurs or the read nbt does not match the format written by {@link #writeTemplate(ITemplate, OutputStream, boolean)}
      */
     @Nullable
-    public static ITemplate readTemplate(InputStream stream, @Nullable TemplateHeader header, boolean persisted) throws IOException {
-        return readTemplate(CompressedStreamTools.readCompressed(stream), header, persisted);
+    public static ITemplate readTemplate(InputStream stream, @Nullable TemplateHeader header, boolean persisted) throws IllegalTemplateFormatException {
+        try {
+            return readTemplate(CompressedStreamTools.readCompressed(stream), header, persisted);
+        } catch (IOException e) {
+            throw new DataCannotBeReadException(e);
+        }
     }
 
     /**
@@ -62,60 +76,57 @@ public final class TemplateIO {
      * @param persisted whether this was written as persisted.
      * @param header    The TemplateHeader if present. Null otherwise.
      * @return A Template if the serializer is known. Null if not.
-     * @throws IOException if a read error occurs or the read nbt does not match the format written by {@link #writeTemplate(ITemplate, OutputStream, boolean)}
+     * @throws IllegalTemplateFormatException if a read error occurs or the read nbt does not match the format written by {@link #writeTemplate(ITemplate, OutputStream, boolean)}
      */
     @Nullable
     public static ITemplate readTemplate(CompoundNBT nbt, @Nullable TemplateHeader header, boolean persisted) throws IllegalTemplateFormatException {
         if (! nbt.contains(NBTKeys.KEY_SERIALIZER, NBT.TAG_STRING))
-            throw new IllegalTemplateFormatException("Cannot read Template without a Serializer!");
+            throw new MissingSerializerException(nbt);
         ITemplateSerializer serializer = Registries.getTemplateSerializer(nbt.getString(NBTKeys.KEY_SERIALIZER));
         if (serializer == null)
             return null;
         if (! nbt.contains(NBTKeys.KEY_DATA, NBT.TAG_COMPOUND))
-            throw new IllegalTemplateFormatException("Cannot read Template without Template Data!");
+            throw new MissingDataException(nbt);
         return serializer.deserialize(nbt.getCompound(NBTKeys.KEY_DATA), header, persisted);
     }
 
-    public static void writeTemplateJson(ITemplate template, OutputStream stream) throws IOException {
-        String json = writeTemplateJson(template);
-        new OutputStreamWriter(stream).append(json);
+    public static void writeTemplateJson(ITemplate template, OutputStream stream) throws IllegalTemplateFormatException {
+        writeTemplateJson(template, stream, null);
     }
 
-    public static String writeTemplateJson(ITemplate template) {
-        try {
-            return GSON.toJson(TemplateJsonRepresentation.ofTemplate(template, null));
-        } catch (IOException e) { //TODO exceptions
-            return null;
-        }
+    public static void writeTemplateJson(ITemplate template, OutputStream stream, @Nullable IBuildOpenOptions openOptions) throws IllegalTemplateFormatException {
+        GSON.toJson(TemplateJsonRepresentation.ofTemplate(template, openOptions), new OutputStreamWriter(stream));
     }
 
-    public static ITemplate readTemplateFromJson(String json) {
-        try {
-            return GSON.fromJson(json, TemplateJsonRepresentation.class).getTemplate();
-        } catch (IOException e) { //TODO exceptions
-            return null;
-        }
+    public static String writeTemplateJson(ITemplate template) throws IllegalTemplateFormatException {
+        return writeTemplateJson(template, (IBuildOpenOptions) null);
     }
 
-    public static ITemplate readTemplateFromJson(InputStream stream) throws IOException {
-        Reader r = new InputStreamReader(stream);
-        StringBuilder builder = new StringBuilder();
-        char[] buf = new char[4096];
-        int read = 0;
-        while ((read = r.read(buf)) > 0)
-            builder.append(buf, 0, read);
-        return readTemplateFromJson(builder.toString());
+    public static String writeTemplateJson(ITemplate template, @Nullable IBuildOpenOptions openOptions) throws IllegalTemplateFormatException {
+        return GSON.toJson(TemplateJsonRepresentation.ofTemplate(template, openOptions));
+    }
+
+    public static ITemplate readTemplateFromJson(String json) throws IllegalTemplateFormatException {
+        return GSON.fromJson(json, TemplateJsonRepresentation.class).getTemplate();
+    }
+
+    public static ITemplate readTemplateFromJson(InputStream stream) throws IllegalTemplateFormatException {
+        return GSON.fromJson(new InputStreamReader(stream), TemplateJsonRepresentation.class).getTemplate();
     }
 
     private static final class TemplateJsonRepresentation {
-        public static TemplateJsonRepresentation ofTemplate(ITemplate template, @Nullable IBuildOpenOptions openOptions) throws IOException {
+        public static TemplateJsonRepresentation ofTemplate(ITemplate template, @Nullable IBuildOpenOptions openOptions) throws IllegalTemplateFormatException {
             ITemplateSerializer serializer = template.getSerializer();
             TemplateHeader header = openOptions != null ?
                     serializer.createHeaderAndTryForceMaterials(template, openOptions) :
                     serializer.createHeaderFor(template);
             CompoundNBT nbt = serializer.serialize(template, true);
             ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            CompressedStreamTools.writeCompressed(nbt, baos);
+            try {
+                CompressedStreamTools.writeCompressed(nbt, baos);
+            } catch (IOException e) {
+                throw new DataCannotBeWrittenException(e, serializer.getRegistryName(), nbt);
+            }
             String base64 = Base64.getEncoder().encodeToString(baos.toByteArray());
             return new TemplateJsonRepresentation(header, base64);
         }
@@ -136,12 +147,18 @@ public final class TemplateIO {
             return body;
         }
 
-        @Nullable
-        private ITemplate getTemplate() throws IOException {
+        private ITemplate getTemplate() throws IllegalTemplateFormatException {
             byte[] bytes = Base64.getDecoder().decode(body);
-            CompoundNBT nbt = CompressedStreamTools.readCompressed(new ByteArrayInputStream(bytes));
+            CompoundNBT nbt;
+            try {
+                nbt = CompressedStreamTools.readCompressed(new ByteArrayInputStream(bytes));
+            } catch (IOException e) {
+                throw new CorruptTemplateStringException(e, header.getSerializer(), body);
+            }
             ITemplateSerializer serializer = Registries.getTemplateSerializer(header.getSerializer());
-            return serializer != null ? serializer.deserialize(nbt, header, true) : null;
+            if (serializer == null)
+                throw new UnknownSerializerException(header.getSerializer());
+            return serializer.deserialize(nbt, header, true);
         }
     }
 }
