3b738a653d61cac79c357e96e75683043215397c
Major_Tuvok <kevin.iselborn@t-online.de>
2019-10-17 20:49:26 +0200

    Remove do not use badge

--- src/main/java/com/direwolf20/buildinggadgets/common/blocks/TemplateManager.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/blocks/TemplateManager.java
@@ -5,11 +5,8 @@ import com.direwolf20.buildinggadgets.common.capability.CapabilityTemplate;
 import com.direwolf20.buildinggadgets.common.registry.OurBlocks;
 import com.direwolf20.buildinggadgets.common.tiles.TemplateManagerTileEntity;
 import com.direwolf20.buildinggadgets.common.util.GadgetUtils;
-import com.direwolf20.buildinggadgets.common.util.lang.Styles;
-import com.direwolf20.buildinggadgets.common.util.lang.TooltipTranslation;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.BlockItemUseContext;
@@ -22,14 +19,12 @@ import net.minecraft.util.Direction;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
-import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.PacketDistributor;
 import net.minecraftforge.items.CapabilityItemHandler;
 
 import javax.annotation.Nullable;
-import java.util.List;
 
 public class TemplateManager extends Block {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
@@ -37,12 +32,6 @@ public class TemplateManager extends Block {
     public TemplateManager(Properties builder) {
         super(builder); //all mc blocks use south as the default...
         setDefaultState(getStateContainer().getBaseState().with(FACING, Direction.SOUTH));
-    }
-
-    @Override
-    public void addInformation(ItemStack stack, @Nullable IBlockReader worldIn, List<ITextComponent> tooltip, ITooltipFlag flagIn) {
-        super.addInformation(stack, worldIn, tooltip, flagIn);
-        tooltip.add(TooltipTranslation.DONOTUSE_TEXT.componentTranslation().setStyle(Styles.RED));
     }
 
     @Override
