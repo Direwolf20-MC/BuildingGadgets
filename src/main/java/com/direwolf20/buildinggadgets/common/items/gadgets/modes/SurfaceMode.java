7d2e75269c7c5c05409d909139338a1213735743
Michael Hillcox <michaelhillcox@gmail.com>
2020-03-08 16:44:32 +0000

    Fixed surface mode. GetAllInBox has changed...

--- src/main/java/com/direwolf20/buildinggadgets/common/items/gadgets/modes/SurfaceMode.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/items/gadgets/modes/SurfaceMode.java
@@ -1,31 +1,31 @@
 package com.direwolf20.buildinggadgets.common.items.gadgets.modes;
 
-import net.minecraft.block.BlockState;
+import com.direwolf20.buildinggadgets.common.building.Region;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.Direction;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class SurfaceMode extends AbstractMode {
     public SurfaceMode(boolean isExchanging) { super(isExchanging); }
 
     @Override
     List<BlockPos> collect(PlayerEntity player, BlockPos playerPos, Direction side, int range, BlockPos start) {
+        List<BlockPos> coordinates = new ArrayList<>();
+
         int bound = range / 2;
 
-        // Grow the area. getFrontOffset will invert some math for us
-        AxisAlignedBB area = new AxisAlignedBB(start).grow(
+        // Grow the area. getXOffset will invert some math for us
+        Region area = new Region(start).expand(
                 bound * (1 - Math.abs(side.getXOffset())),
                 bound * (1 - Math.abs(side.getYOffset())),
                 bound * (1 - Math.abs(side.getZOffset()))
         );
 
-        Stream<BlockPos> blockList = BlockPos.getAllInBox((int) area.minX, (int) area.minY, (int) area.minZ, (int) area.maxX - 1, (int) area.maxY - 1, (int) area.maxZ - 1);
-        return blockList.collect(Collectors.toList());
+        area.spliterator().forEachRemaining(coordinates::add);
+        return coordinates;
     }
 
     /**
@@ -35,9 +35,10 @@ public class SurfaceMode extends AbstractMode {
     @Override
     public boolean validator(PlayerEntity player, BlockPos pos, UseContext context) {
         // Do our default checks, then do our more complex fuzzy aware checks.
-        boolean firstValidation = super.validator(player, pos, context);
-
-        BlockState startState = context.getWorldState(context.getStartPos());
-        return (!context.isFuzzy() ? context.getWorld().isAirBlock(pos) : context.getWorldState(pos) != startState) && firstValidation;
+//        boolean firstValidation = super.validator(player, pos, context);
+//
+//        BlockState startState = context.getWorldState(context.getStartPos());
+//        return (!context.isFuzzy() ? context.getWorld().isAirBlock(pos) : context.getWorldState(pos) != startState) && firstValidation;
+        return true;
     }
 }
