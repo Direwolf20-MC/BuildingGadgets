d89f3459c0a73c5684b69be80a71380ed6e988d6
Michael <hello.michaelhillcox@gmail.com>
2020-06-07 14:45:33 +0100

    Basically a new mod setup.

---
+++ src/main/java/com/direwolf20/buildinggadgets/BuildingGadgets.java
@@ -0,0 +1,50 @@
+package com.direwolf20.buildinggadgets;
+
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
+import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(BuildingGadgets.MOD_ID)
+public class BuildingGadgets
+{
+    public static final String MOD_ID = "buildinggadgets";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public BuildingGadgets() {
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        eventBus.addListener(this::setup);
+        eventBus.addListener(this::clientSetup);
+        eventBus.addListener(this::enqueueIMC);
+        eventBus.addListener(this::processIMC);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+
+    }
+
+    private void clientSetup(final FMLClientSetupEvent event) {
+
+    }
+
+    private void enqueueIMC(final InterModEnqueueEvent event)
+    {
+//        // some example code to dispatch IMC to another mod
+//        InterModComms.sendTo("examplemod", "helloworld", () -> { LOGGER.info("Hello world from the MDK"); return "Hello world";});
+    }
+
+    private void processIMC(final InterModProcessEvent event)
+    {
+//        // some example code to receive and process InterModComms from other mods
+//        LOGGER.info("Got IMC {}", event.getIMCStream().
+//                map(m->m.getMessageSupplier().get()).
+//                collect(Collectors.toList()));
+    }
+}
