355560e974a2e4acba9158816f7809de13a7cbd4
Michael <michaelhillcox@gmail.com>
2020-07-27 18:38:03 +0100

    Removed dead code

--- src/main/java/com/direwolf20/buildinggadgets/common/building/placement/IPositionPlacementSequence.java
+++ src/main/java/com/direwolf20/buildinggadgets/common/building/placement/IPositionPlacementSequence.java
@@ -21,7 +21,6 @@ public interface IPositionPlacementSequence extends IPlacementSequence<BlockPos>
      * {@inheritDoc}
      * <p>
      * In general it is not necessary to make any considerations whether a given position is suitable for placement, however an implementation may make exceptions from this rule.
-     * See {@link PlacementSequences.ConnectedSurface ConnectedSurface} for further information.
      */
     @Override
     Iterator<BlockPos> iterator();
